08162e4abe39bbc928698e05f61248e160ec233c
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2022-09-05 01:51:44 -0300

    cuida da criação de links.

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,9 @@ fn main() {
    else 
       { visualizacao_padrao(limpeza); }
 
+   // criando executáveis se não houver.
+   linka_executaveis();
+
 }
 
 fn visualizacao_padrao(mut fila:FilaExclusao) {
@@ -63,5 +66,74 @@ fn visualizacao_padrao(mut fila:FilaExclusao) {
       );
       // a cada tempo delimitado.
       thread::sleep(Duration::from_secs_f32(17.7));
+   }
+}
+
+/* cria link simbólico tanto para a versão em
+ * debug, quanto para o binário final. */
+use std::os::unix::fs::symlink;
+use std::env::current_exe;
+use std::path::{Path, PathBuf};
+
+// complementa link ao executável.
+fn computa_caminho(caminho_str:&str) -> PathBuf {
+   // à partir do caminho do executável ...
+   match current_exe() {
+      Ok(mut base) => {
+         // remove executável do caminho.
+         base.pop(); 
+         // sai do subdiretório 'release'.
+         base.pop(); 
+         // sai do subdiretório 'target'.
+         base.pop();
+         // complementa com o caminho passado.
+         base.push(caminho_str);
+         return base;
+      } Err(_) =>
+         { panic!("não foi possível obter o caminho do executável!"); }
+   }
+}
+
+fn linka_executaveis() {
+   // caminho aos executáveis.
+   let caminho_str = "target/release/limpa_downloads";
+   let executavel = computa_caminho(caminho_str);
+   let executavel_debug: PathBuf;
+   let caminho_str = "target/debug/limpa_downloads";
+   executavel_debug = computa_caminho(caminho_str);
+
+   // seus links simbólicos:
+   let ld_link = computa_caminho("LD");
+   let ld_debug_link = computa_caminho("LD_debug");
+
+   if ld_link.as_path().exists() && 
+   ld_link.as_path().is_symlink() {
+      if executavel.as_path().exists() 
+         { println!("binário do executável existe."); }
+      println!("LD já existe.");
+   } else {
+      match symlink(executavel.as_path(), ld_link.as_path()) {
+         Ok(_) => {
+            print!("criando LD ... ");
+            println!("com sucesso.");
+         } Err(erro) => 
+            { println!("erro:[{}]", erro); }
+      };
+   }
+
+   if ld_debug_link.as_path().exists() && 
+   ld_link.as_path().is_symlink() { 
+      println!("LD(debug) já existe.");
+      if executavel_debug.exists() 
+         { println!("binário do executável(DEBUG) existe."); }
+   } else {
+      print!("criando LD(DEBUG) ... ");
+      match symlink(executavel_debug.as_path(), ld_debug_link.as_path()) {
+         Ok(_) => {
+            print!("criando LD(debug) ... ");
+            println!("com sucesso.");
+         } Err(erro) => 
+            { println!("erro:[{}]", erro); }
+      };
    }
 }
