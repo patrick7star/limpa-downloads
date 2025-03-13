069bca9c5a8df1a750e811b743f20d28a921d14d
Charles Montgomery Burns <90501975+patrick7star@users.noreply.github.com>
2025-03-13 15:14:07 -0300

    lista de exclusão por JSON, e linque no repoisitório próprio..
    A questão da geração de linques no repositório de links(`$LINKS`) foi consertada, já a questão da exclusão está escrita no código também foi consertada, todos os itens e suas respectivas `validades` tem que ser definidas no arquivo JSON do projeto.

--- src/item_de_exclusao.rs
+++ src/item_de_exclusao.rs
@@ -1,23 +1,24 @@
+extern crate utilitarios;
 
-// biblioteca padrão do Rust:
+mod remocao_dir;
+mod validades;
+
+// Biblioteca padrão do Rust:
 use std::path::{Path, PathBuf};
 use std::time::{SystemTime, Duration};
 use std::ops::Drop;
 use std::fs::{remove_dir, remove_file, read_dir};
-use std::fmt::{
-   Display, Formatter, 
-   Result as Formato
-};
+use std::fmt::{ Display, Formatter, Result as Formato };
 use std::string::String;
-
-// biblioteca externa:
-extern crate utilitarios;
-use utilitarios::progresso::temporizador_progresso;
-use super::letreiro::Letreiro;
-
-// próprio módulo.
-mod remocao_dir;
+// Módulos destes projeto:
 use remocao_dir::{ self as RD, };
+use super::letreiro::Letreiro;
+use validades::*;
+// Biblioteca externa:
+use utilitarios::legivel::{
+   tamanho as tamanho_legivel, 
+   tempo as tempo_legivel
+};
 
 
 /* Trait para re-implementação do Drop com "saída" na tela. Está versão é 
@@ -29,12 +30,16 @@ trait DropPadrao
 pub struct Item {
    // caminho para o item.
    pub caminho: PathBuf,
+
    // nome do item em sí.
    pub nome: String,
+
    // tempo restantes de vida(na fila de exclusão).
    pub validade: Duration,
+
    // o último acesso do itém.
    pub ultimo_acesso: SystemTime,
+
    // letreiro dinâmico, muito para o modo gráfico.
    pub letreiro: Letreiro
 }
@@ -45,11 +50,7 @@ impl Item {
    validade:Duration) -> Self {
       // extraí nomes do caminho.
       let nome:String = {
-         caminho.as_path()
-         .file_name()
-         .unwrap()
-         .to_str()
-         .unwrap()
+         caminho.as_path().file_name().unwrap().to_str().unwrap()
          .to_string()
       };
       // letreiro com letras(do nome) em movimento.
@@ -97,19 +98,26 @@ impl Item {
 
 // impressão sobre o status do ítem.
 impl Display for Item {
-   fn fmt(&self, formatador:&mut Formatter<'_>) -> Formato {
-      let duracao = match self.ultimo_acesso.elapsed() {
+
+   fn fmt(&self, formatador:&mut Formatter<'_>) -> Formato 
+   {
+      let inicio = match self.ultimo_acesso.elapsed() {
          Ok(ultimo_acesso) => ultimo_acesso,
          Err(estimativa) => estimativa.duration()
       };
-      let barra_de_progresso:String = {
-         temporizador_progresso(
-            self.nome.as_str(),
-            duracao,
-            self.validade
-         )
-      };
-      write!(formatador, "{}", barra_de_progresso)
+      let decorrido = self.validade - inicio;
+      let restante = tempo_legivel(decorrido.as_secs(), true);
+      // Cálculo do percentual pra exclusão.
+      let a = decorrido.as_secs_f32();
+      let b = self.validade.as_secs_f32();
+      let percentual = (1.0 - a / b) * 100.0;
+      // Tamanho do objeto à excluir.
+      let size = self.caminho.metadata().unwrap().len();
+
+      write!(
+         formatador, "{:<60}~ {3:<11}| {:<9}|{:6.1}%", 
+         self.nome, restante, percentual, tamanho_legivel(size, true)
+      )
    }
 }
 
@@ -119,20 +127,15 @@ impl Drop for Item {
    fn drop(&mut self) {
       if self.expirado() {
          let caminho = self.caminho.as_path();
-         if caminho.is_file() {
-            match remove_file(caminho) {
-               Ok(_) => (),
-               Err(_) => 
-                  { panic!("[ERRO!!!] o arquivo ainda continua!"); } 
-            };
-         }
+         if caminho.is_file() 
+            { remove_file(caminho).unwrap(); }
          else if caminho.is_dir() && diretorio_esta_vazio(caminho)
          // Caso específico para pastas vázias:
             { remove_dir(caminho).unwrap(); }
-
+         else
          /* Diretório com conteúdo(pastas e arquivos, e também subdiretórios
           * com mais arquivos e pastas). */
-         remocao_dir::remocao_completa(caminho);
+          { remocao_dir::remocao_completa(caminho); }
       }
    }
 }
@@ -142,7 +145,7 @@ impl DropPadrao for Item {
    fn drop(&mut self) {
       print!("==> removendo \"{}\"... ", self.nome); 
       // remoção do arquivo.
-      drop(self);
+      let _= self;
       println!("realização sucedida.");
    }
 }
@@ -166,56 +169,9 @@ impl FilaExclusao {
       self.proximas_exclusao.is_empty() 
    }
 
-   // Computa a devida 'duração' para a devida extensão.
-   fn duracao_para_devida_extensao(extension: &str) -> Duration {
-      if extension == "iso" {
-         const MESES:u64 = (2.7*30.0*24.0*3600.0) as u64;
-         Duration::from_secs(MESES)
-      } else if extension == "zip" {
-         const ALGUNS_HORAS:u64 = (4.8 * 3600.0) as u64;
-         Duration::from_secs(ALGUNS_HORAS)
-      } else if extension == "ttf" || extension == "deb" {
-         const ALGUNS_MINUTOS:u64 = (32.3 * 60.0) as u64;
-         Duration::from_secs(ALGUNS_MINUTOS)
-      } else if extension == "pdf" {
-         const ALGUNS_HORAS:u64 = (9.6 * 3600.0) as u64;
-         Duration::from_secs(ALGUNS_HORAS)
-      } else if extension == "torrent" {
-         const ALGUNS_MINUTOS:u64 = (45.2 * 60.0) as u64;
-         Duration::from_secs(ALGUNS_MINUTOS)
-      } else if extension == "dat" || extension == "djvu" ||
-      extension == "toml" {
-         const ALGUNS_MINUTOS:u64 = (7.2 * 60.0) as u64;
-         Duration::from_secs(ALGUNS_MINUTOS)
-      } else if extension == "epub" {
-         const ALGUNS_DIAS:u64 = (9.6 * 24.0 * 3600.0) as u64;
-         Duration::from_secs(ALGUNS_DIAS)
-      } else if extension == "tar" || extension == "gz" {
-         const ALGUNS_HORAS:u64 = (3.8 * 3600.0) as u64;
-         Duration::from_secs(ALGUNS_HORAS)
-      } else {
-         const PADRAO:u64 = (5.9 * 3600.0) as u64;
-         Duration::from_secs(PADRAO)
-      }
-   }
-
-   // Computa duração para um diretório, seja qual for seu estado.
-   fn duracao_para_diretorio(caminho: &Path) -> Duration {
-      const DIA: f32 = 24.0 * 3600.0;
-      const ALGUNS_DIAS:u64 = (DIA * 13.9) as u64;
-
-      // criando o ítem e adicionando na lista.
-      if diretorio_esta_vazio(caminho)
-         { Duration::from_secs(5 * 60) }
-      else 
-         { Duration::from_secs(ALGUNS_DIAS) }
-   }
-
    /// Visualiza e opera possível exclusão.
    pub fn visualiza(&mut self) {
-      /* pondo 'Item's que estão prestes a 
-       * ser deletados, na fila de exclusão.
-       */
+      // Pondo 'Item's, prestes a ser deletados, na fila de exclusão.
       let mut qtd = self.todos.len();
       while qtd > 0 {
          // na fila de exclusão de hoje.
@@ -237,12 +193,13 @@ impl FilaExclusao {
       // lista de exclusão.
       let mut referencias: Vec<&mut Item>;
       referencias = Vec::with_capacity(qtd);
-      /* não se mostra expirados, serão excluídos
-       * em seguida, aqui serão colocadas na lista
-       * de exclusão. */
+
+      println!("\nTodos itens que serão excluídos hoje:\n");
+      /* Não se mostra expirados, serão excluídos em seguida, aqui serão 
+       * colocadas na lista de exclusão. */
       for item in self.proximas_exclusao.iter_mut() { 
          if !item.expirado()
-            { println!("{}", item); }
+            { println!("  {}", item); }
          else
             { referencias.push(item); }
       }
@@ -266,7 +223,7 @@ impl FilaExclusao {
 
          // No caso de se for um diretório.
          if caminho.is_dir() {
-            let validade = Self::duracao_para_diretorio(&caminho);
+            let validade = duracao_para_diretorio(&caminho);
             let auxiliar = SystemTime::now();
             let ua_medio = RD::acesso_medio_dir(&caminho);
             let tempo = Duration::from_secs(ua_medio as u64);
@@ -284,7 +241,8 @@ impl FilaExclusao {
          // Caso geral: um arquivo com extensão.
          if let Some(os_str) = caminho.extension() {
             if let Some(ext) = os_str.to_str() {
-               let validade = Self::duracao_para_devida_extensao(ext);
+               // let validade = Self::duracao_para_devida_extensao(ext);
+               let validade = duracao_para_devida_extensao_por_json(ext);
 
                if let Ok(ua) = entry.metadata().unwrap().accessed()
                   { lista.push(Item::cria(caminho, ua, validade)); }
@@ -335,7 +293,3 @@ fn diretorio_esta_vazio(caminho:&Path) -> bool {
 }
 
 
-#[cfg(test)]
-mod tests {
-   // use super::*;
-}
--- src/links.rs
+++ src/links.rs
@@ -1,15 +1,22 @@
 
 /* cria link simbólico tanto para a versão em debug, quanto para o 
  * binário final. */
-#[cfg(target_os="unix")]
+#[cfg(target_os="linux")]
 use std::os::unix::fs::symlink;
 use std::env::current_exe;
 use std::path::PathBuf;
+use std::ffi::{OsStr};
+use std::path::{Path, Component};
+use std::env::{var, VarError};
+use std::io;
 
-// complementa link ao executável.
+
+/// Computa o caminho até o projeto, baseado que, tal executável deve está
+/// no lugar comum onde uma simples compilação do Rust o faz se não for 
+/// definida diferente. Portanto em algum subdiretório do diretório 'target'.
 pub fn computa_caminho(caminho_str:&str) -> PathBuf {
-   // à partir do caminho do executável ...
    match current_exe() {
+   // à partir do caminho do executável ...
       Ok(mut base) => {
          // remove executável do caminho.
          base.pop(); 
@@ -23,6 +30,34 @@ pub fn computa_caminho(caminho_str:&str) -> PathBuf {
       } Err(_) =>
          { panic!("não foi possível obter o caminho do executável!"); }
    }
+}
+
+fn cria_linques_no_repositorio(nome_do_linque: &str) -> io::Result<PathBuf> 
+{
+   let caminho_do_executavel = current_exe()?;
+   let caminho_repositorio = {
+      match var("LINKS") {
+         Ok(data) => Ok(data),
+         Err(tipo_de_erro) => {
+            let erro_a = io::ErrorKind::InvalidInput;
+            let erro_b = io::ErrorKind::InvalidData;
+
+            match tipo_de_erro {
+               VarError::NotPresent => Err(erro_a),
+               VarError::NotUnicode(_) => Err(erro_b)
+            }
+         }
+      }
+   }?;
+   let fonte = &caminho_do_executavel;
+   let destino = Path::new(&caminho_repositorio).join(nome_do_linque);
+   let bate = Component::Normal(OsStr::new("release"));
+
+   // Verificação se estamos falando apenas da parte 'release'.
+   assert!(fonte.components().any(|part| part == bate));
+   println!("fonte: {:?}\ndestino: {:?}", fonte, destino);
+   symlink(fonte, &destino)?;
+   Ok(destino)
 }
 
 pub fn linka_executaveis(nome_do_linque: &str) {
@@ -39,7 +74,6 @@ pub fn linka_executaveis(nome_do_linque: &str) {
    }
 
    // caminho aos executáveis.
-   // let caminho_ao_executavel = "target/release/limpa_downloads";
    let executavel = computa_caminho(caminho_ao_executavel);
    // caminho do linque para o executável.
    let linque_otimizado = computa_caminho(nome_do_linque);
@@ -50,7 +84,7 @@ pub fn linka_executaveis(nome_do_linque: &str) {
       println!("{}", mensagem_i); 
    } else {
       print!("como não existe, criando '{}' ... ", nome_do_linque);
-      #[cfg(target_os="unix")]
+      #[cfg(target_os="linux")]
       let resultado_criacao_do_link = symlink(
          executavel.as_path(), 
          linque_otimizado.as_path()
@@ -66,5 +100,32 @@ pub fn linka_executaveis(nome_do_linque: &str) {
             println!("erro ao tentar criar linque!!!");
          }
       };
+   }
+
+   match cria_linques_no_repositorio(nome_do_linque) {
+      Ok(caminho) => { 
+         assert!(caminho.exists()); 
+         println!("Linque criado com sucesso em $LINKS."); 
+      } Err(classificacao_do_erro) => { 
+         match classificacao_do_erro.kind() {
+            io::ErrorKind::AlreadyExists =>
+               { println!("Já existe um linque em $LINKS."); }
+            _ =>
+               { panic!("{}", classificacao_do_erro); }
+         }
+      } 
+   }
+}
+
+#[cfg(test)]
+mod tests {
+   use std::fs::{remove_file};
+
+   #[test]
+   fn criacao_de_linque_no_repositorio_de_linques() {
+      let nome = "linque-do-limpa-downloads-teste";
+      let out = super::cria_linques_no_repositorio(nome).unwrap();
+
+      assert!(remove_file(out).is_ok());
    }
 }
